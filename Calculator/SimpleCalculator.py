9bc5cbf306afeedbadf1e2610d06b72a25d2a134
Joshua Yadao <joshuawyadao@gmail.com>
2024-01-13 15:30:11 -0800

    Create skeleton and documentation for SimpleCalculator.py

---
+++ Calculator/SimpleCalculator.py
@@ -0,0 +1,63 @@
+""" SimpleCalculator.py
+
+Calculator to do simple arithmetic:
+    - Addition (+)
+    - Subtraction (-)
+    - Multiplication (*)
+    - Division (/)
+
+"""
+class SimpleCalculator:
+    def __init__(self) -> None:
+        pass
+    
+    def __str__(self) -> str:
+        pass
+    
+    def add(x:int, y:int) -> int:
+        """Adds two numbers together
+
+        Args:
+            x (int): summand 
+            y (int): summand
+
+        Returns:
+            int: sum
+        """
+        pass
+    
+    def subtract(x:int, y:int) -> int:
+        """Subtract two numbers together
+
+        Args:
+            x (int): minuend
+            y (int): subtrahend
+
+        Returns:
+            int: difference
+        """
+        pass
+    
+    def multiply(x:int, y:int) -> int:
+        """Multiply two numbers together
+
+        Args:
+            x (int): multiplier
+            y (int): multiplicand
+
+        Returns:
+            int: product
+        """
+        pass
+    
+    def divide(x:int, y:int) -> int:
+        """Divides two numbers together
+
+        Args:
+            x (int): dividend/numerator
+            y (int): divisor/denomincator
+
+        Returns:
+            int: quotient
+        """
+        pass

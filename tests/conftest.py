a649616cafa60bc44607b5fb96c4494afd6d3963
Joshua Yadao <joshuawyadao@gmail.com>
2024-01-13 17:32:25 -0800

    Created fixtures within conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,68 @@ Configuration file for PyTest
 
 import pytest
 
-### Fixtures for UNIT TESTS ###
+### INTEGER INPUTS ###
 
-# Integer Inputs
+# Positive INT value
+@pytest.fixture
+def positive_int():
+    return 5
 
-# Double/float Inputs
+# Negative INT value
+@pytest.fixture
+def negative_int():
+    return -3
 
-# String Inputs
+# Zero INT value
+@pytest.fixture
+def zero_int():
+    return 0
 
-# Combination of Data Types Inputs
+### FLOAT INPUTS ###
+
+# Positive FLOAT value
+@pytest.fixture
+def postive_float():
+    return 7.21
+
+# Negative FLOAT value
+@pytest.fixture
+def negative_float():
+    return -2.38
+
+# Zero FLOAT value
+@pytest.fixture
+def zero_float():
+    return 0.00
+
+### STRING INPUTS ###
+
+# Single CHAR value
+@pytest.fixture
+def single_char():
+    return "a"
+
+# STRING value
+@pytest.fixture
+def string_val():
+    return "test"
+
+# STRING of an positive INT value
+@pytest.fixture
+def string_positive_int():
+    return "9"
+
+# STRING of an negative INT value
+@pytest.fixture
+def string_negative_int():
+    return "-8"
+
+# STRING of an positive FLOAT value
+@pytest.fixture
+def string_positive_float():
+    return "6.45"
+
+# STRING of an negative FLOAT value
+@pytest.fixture
+def string_negative_float():
+    return "-4.73"

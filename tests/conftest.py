438410f7da6de201dedd041582d2dea4f62885d0
Joshua Yadao <joshuawyadao@gmail.com>
2024-01-13 16:09:43 -0800

    Created conftest.py to contain all of the fixtures and parameterization for the unit test suite

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+""" ConfTest.py
+
+Configuration file for PyTest
+
+"""
+
+import pytest
+
+### Fixtures for UNIT TESTS ###
+
+# Integer Inputs
+
+# Double/float Inputs
+
+# String Inputs
+
+# Combination of Data Types Inputs

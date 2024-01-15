3fa64e0291ebfa9ea97011f71f3f53cb63ae1a00
Joshua Yadao <joshuawyadao@gmail.com>
2024-01-15 11:58:22 -0800

    Created all test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,31 +30,79 @@ def neg_int_pos_int():
 
 # Two zeros
 @pytest.fixture
-def zero_zero():
+def zero_int_zero_int():
     return 0, 0
 
 # One positive, one zero
 @pytest.fixture
-def pos_int_zero():
+def pos_int_zero_int():
     return 1, 0
 
 # One zero, one positive
 @pytest.fixture
-def zero_pos_int():
+def zero_int_pos_int():
     return 0, 8
 
 # One negative, one zero
 @pytest.fixture
-def neg_int_zero():
+def neg_int_zero_int():
     return -6, 0
 
 # One zero, one negative
 @pytest.fixture
-def neg_int_zero():
+def neg_int_zero_int():
     return 0, -2
 
-### FLOAT INPUTS ###
-#TODO: Add in permutations for FLOAT inputs
-
 ### STRING INPUTS ###
-#TODO: Add in permutations for String inputs
+
+# Two chars
+@pytest.fixture
+def char_char():
+    return "a", "c"
+
+# Two strings
+@pytest.fixture
+def str_str():
+    return "test", "help"
+
+# One char, one string
+@pytest.fixture
+def char_str():
+    return "x", "math"
+
+# One string, one char
+@pytest.fixture
+def str_char():
+    return "happy", "v"
+
+## Combination of String and INT inputs ##
+
+# One positive int, one positive string
+@pytest.fixture
+def pos_int_str():
+    return 9, "1"
+
+# One positive string, one positive int
+@pytest.fixture
+def str_pos_int():
+    return "3", 5
+
+# One neg int, one positive string
+@pytest.fixture
+def neg_int_str():
+    return -9, "1"
+
+# One string, one neg int
+@pytest.fixture
+def str_neg_int():
+    return "3", -5
+
+# One zero int, one string
+@pytest.fixture
+def zero_int_str():
+    return 0, "-4"
+
+# One string, one zero int
+@pytest.fixture
+def str_zero_int():
+    return "-6", 0

8eb7066113bb0a3891d42beabdcccbdcc482a2c8
Joshua Yadao <joshuawyadao@gmail.com>
2024-01-16 17:26:47 -0800

    Updated comments to make it easier to read at a glance

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,123 +6,123 @@ Configuration file for PyTest
 
 import pytest
 
-### INTEGER INPUTS ###
+### 1. INTEGER INPUTS ###
 
-# Two positive
+# 1.1. Two positive
 @pytest.fixture(scope="session")
 def pos_int_pos_int():
     return 5, 2
 
-# Two negative
+# 1.2. Two negative
 @pytest.fixture(scope="session")
 def neg_int_neg_int():
     return -3, -6
 
-# One positive, one negative
+# 1.3. One positive, one negative
 @pytest.fixture(scope="session")
 def pos_int_neg_int():
     return 9, -2
 
-# One negative, one positive
+# 1.4. One negative, one positive
 @pytest.fixture(scope="session")
 def neg_int_pos_int():
     return -7, 1
 
-# Two zeros
+# 1.5. Two zeros
 @pytest.fixture(scope="session")
 def zero_int_zero_int():
     return 0, 0
 
-# One positive, one zero
+# 1.6. One positive, one zero
 @pytest.fixture(scope="session")
 def pos_int_zero_int():
     return 1, 0
 
-# One zero, one positive
+# 1.7. One zero, one positive
 @pytest.fixture(scope="session")
 def zero_int_pos_int():
     return 0, 8
 
-# One negative, one zero
+# 1.8. One negative, one zero
 @pytest.fixture(scope="session")
 def neg_int_zero_int():
     return -6, 0
 
-# One zero, one negative
+# 1.9. One zero, one negative
 @pytest.fixture(scope="session")
 def zero_int_neg_int():
     return 0, -2
 
-### STRING INPUTS ###
+### 2. STRING INPUTS ###
 
-# Two chars
+# 2.1. Two chars
 @pytest.fixture(scope="session")
 def char_char():
     return "a", "c"
 
-# Two strings
+# 2.2. Two strings
 @pytest.fixture(scope="session")
 def str_str():
     return "test", "help"
 
-# One char, one string
+# 2.3. One char, one string
 @pytest.fixture(scope="session")
 def char_str():
     return "x", "math"
 
-# One string, one char
+# 2.4. One string, one char
 @pytest.fixture(scope="session")
 def str_char():
     return "happy", "v"
 
-# Two strings of positive ints
+# 2.5. Two strings of positive ints
 @pytest.fixture(scope="session")
 def pos_str_pos_str():
     return "3", "5"
 
-# Two strings of negative ints
+# 2.6. Two strings of negative ints
 @pytest.fixture(scope="session")
 def neg_str_neg_str():
     return "-7", "-4"
 
-# Two strings of one positive, one negative
+# 2.7. Two strings of one positive, one negative
 @pytest.fixture(scope="session")
 def pos_str_neg_str():
     return "6", "-8"
 
-# Two strings of one negative, one positive
+# 2.8. Two strings of one negative, one positive
 @pytest.fixture(scope="session")
 def neg_str_pos_str():
     return "-2", "5"
 
-### COMBINATION INPUTS ###
+### 3. COMBINATION INPUTS ###
 
-# One positive int, one positive string
+# 3.1. One positive int, one positive string
 @pytest.fixture(scope="session")
 def pos_int_str():
     return 9, "1"
 
-# One positive string, one positive int
+# 3.2. One positive string, one positive int
 @pytest.fixture(scope="session")
 def str_pos_int():
     return "3", 5
 
-# One neg int, one positive string
+# 3.3. One neg int, one positive string
 @pytest.fixture(scope="session")
 def neg_int_str():
     return -9, "1"
 
-# One string, one neg int
+# 3.4. One string, one neg int
 @pytest.fixture(scope="session")
 def str_neg_int():
     return "3", -5
 
-# One zero int, one string
+# 3.5. One zero int, one string
 @pytest.fixture(scope="session")
 def zero_int_str():
     return 0, "-4"
 
-# One string, one zero int
+# 3.6. One string, one zero int
 @pytest.fixture(scope="session")
 def str_zero_int():
     return "-6", 0
--- tests/unit/test_simple_calculator.py
+++ tests/unit/test_simple_calculator.py
@@ -10,78 +10,78 @@ from simple_calculator import SimpleCalculator
 
 ### 1. TEST CASES ###
 
-# 1.1 Addition w/ INT inputs
+# 1.1. Addition w/ INT inputs
 ADD_INT_TEST_CASES = ("add_int, expected_add_int", [
-    ("pos_int_pos_int", 7),     # Add two positive ints
-    ("neg_int_neg_int", -9),    # Add two negative ints
-    ("pos_int_neg_int", 7),     # Add one positive, one negative ints
-    ("neg_int_pos_int", -6),    # Add one negative, one positive ints
-    ("zero_int_zero_int", 0),   # Add two zeros ints
-    ("pos_int_zero_int", 1),    # Add one positive, one zero ints
-    ("zero_int_pos_int", 8),    # Add one zero, one positive ints
-    ("neg_int_zero_int", -6),   # Add one negative, one zero ints
-    ("zero_int_neg_int", -2),   # Add one zero, one negative ints
+    ("pos_int_pos_int", 7),     # 1.1. Add two positive ints
+    ("neg_int_neg_int", -9),    # 1.2. Add two negative ints
+    ("pos_int_neg_int", 7),     # 1.3. Add one positive, one negative ints
+    ("neg_int_pos_int", -6),    # 1.4. Add one negative, one positive ints
+    ("zero_int_zero_int", 0),   # 1.5. Add two zeros ints
+    ("pos_int_zero_int", 1),    # 1.6. Add one positive, one zero ints
+    ("zero_int_pos_int", 8),    # 1.7. Add one zero, one positive ints
+    ("neg_int_zero_int", -6),   # 1.8. Add one negative, one zero ints
+    ("zero_int_neg_int", -2),   # 1.9. Add one zero, one negative ints
 ])
 
-# 1.2 Subtraction w/ INT inputs
+# 1.2. Subtraction w/ INT inputs
 SUB_INT_TEST_CASES = ("sub_int, expected_sub_int", [
-    ("pos_int_pos_int", 3),     # Sub two positive ints
-    ("neg_int_neg_int", 3),     # Sub two negative ints
-    ("pos_int_neg_int", 11),    # Sub one positive, one negative ints
-    ("neg_int_pos_int", -8),    # Sub one negative, one positive ints
-    ("zero_int_zero_int", 0),   # Sub two zeros ints
-    ("pos_int_zero_int", 1),    # Sub one positive, one zero ints
-    ("zero_int_pos_int", -8),   # Sub one zero, one positive ints
-    ("neg_int_zero_int", -6),   # Sub one negative, one zero ints
-    ("zero_int_neg_int", 2),    # Sub one zero, one negative ints
+    ("pos_int_pos_int", 3),     # 1.1. Sub two positive ints
+    ("neg_int_neg_int", 3),     # 1.2. Sub two negative ints
+    ("pos_int_neg_int", 11),    # 1.3. Sub one positive, one negative ints
+    ("neg_int_pos_int", -8),    # 1.4. Sub one negative, one positive ints
+    ("zero_int_zero_int", 0),   # 1.5. Sub two zeros ints
+    ("pos_int_zero_int", 1),    # 1.6. Sub one positive, one zero ints
+    ("zero_int_pos_int", -8),   # 1.7. Sub one zero, one positive ints
+    ("neg_int_zero_int", -6),   # 1.8. Sub one negative, one zero ints
+    ("zero_int_neg_int", 2),    # 1.9. Sub one zero, one negative ints
 ])
 
-# 1.3 Multiplication w/ INT inputs
+# 1.3. Multiplication w/ INT inputs
 MULT_INT_TEST_CASES = ("mult_int, expected_mult_int", [
-    ("pos_int_pos_int", 10),    # Multi two positive ints
-    ("neg_int_neg_int", 18),    # Multi two negative ints
-    ("pos_int_neg_int", -18),   # Multi one positive, one negative ints
-    ("neg_int_pos_int", -7),    # Multi one negative, one positive ints
-    ("zero_int_zero_int", 0),   # Multi two zeros ints
-    ("pos_int_zero_int", 0),    # Multi one positive, one zero ints
-    ("zero_int_pos_int", 0),    # Multi one zero, one positive ints
-    ("neg_int_zero_int", 0),    # Multi one negative, one zero ints
-    ("zero_int_neg_int", 0),    # Multi one zero, one negative ints
+    ("pos_int_pos_int", 10),    # 1.1. Multi two positive ints
+    ("neg_int_neg_int", 18),    # 1.2. Multi two negative ints
+    ("pos_int_neg_int", -18),   # 1.3. Multi one positive, one negative ints
+    ("neg_int_pos_int", -7),    # 1.4. Multi one negative, one positive ints
+    ("zero_int_zero_int", 0),   # 1.5. Multi two zeros ints
+    ("pos_int_zero_int", 0),    # 1.6. Multi one positive, one zero ints
+    ("zero_int_pos_int", 0),    # 1.7. Multi one zero, one positive ints
+    ("neg_int_zero_int", 0),    # 1.8. Multi one negative, one zero ints
+    ("zero_int_neg_int", 0),    # 1.9. Multi one zero, one negative ints
 ])
 
-# 1.4 Division w/ INT inputs
+# 1.4. Division w/ INT inputs
 DIV_INT_TEST_CASES = ("div_int, expected_div_int", [
-    ("pos_int_pos_int", 2.5),                   # Div two positive ints
-    ("neg_int_neg_int", 0.5),                   # Div two negative ints
-    ("pos_int_neg_int", -4.5),                  # Div one positive, one negative ints
-    ("neg_int_pos_int", -7),                    # Div one negative, one positive ints
-    ("zero_int_zero_int", ZeroDivisionError),   # Div two zeros ints
-    ("pos_int_zero_int", ZeroDivisionError),    # Div one positive, one zero ints
-    ("zero_int_pos_int", 0),                    # Div one zero, one positive ints
-    ("neg_int_zero_int", ZeroDivisionError),    # Div one negative, one zero ints
-    ("zero_int_neg_int", 0),                    # Div one zero, one negative ints
+    ("pos_int_pos_int", 2.5),                   # 1.1. Div two positive ints
+    ("neg_int_neg_int", 0.5),                   # 1.2. Div two negative ints
+    ("pos_int_neg_int", -4.5),                  # 1.3. Div one positive, one negative ints
+    ("neg_int_pos_int", -7),                    # 1.4. Div one negative, one positive ints
+    ("zero_int_zero_int", ZeroDivisionError),   # 1.5. Div two zeros ints
+    ("pos_int_zero_int", ZeroDivisionError),    # 1.6. Div one positive, one zero ints
+    ("zero_int_pos_int", 0),                    # 1.7. Div one zero, one positive ints
+    ("neg_int_zero_int", ZeroDivisionError),    # 1.8. Div one negative, one zero ints
+    ("zero_int_neg_int", 0),                    # 1.9. Div one zero, one negative ints
 ])
 
-# 1.5 All arithmetic w/ STR inputs (always result in TypeError)
+# 1.5. All arithmetic w/ STR inputs (always result in TypeError)
 STRING_TEST_CASES = ("arith_str, expected_str", [
-    ("char_char", TypeError),       # Two chars
-    ("str_str", TypeError),         # Two strings
-    ("char_str", TypeError),        # One char, one string
-    ("str_char", TypeError),        # One string, one char
-    ("pos_str_pos_str", TypeError), # Two strings of positive ints
-    ("neg_str_neg_str", TypeError), # Two strings of negative ints
-    ("pos_str_neg_str", TypeError), # Two strings of one positive, one negative
-    ("neg_str_pos_str", TypeError), # Two strings of one negative, one positive
+    ("char_char", TypeError),       # 2.1. Two chars
+    ("str_str", TypeError),         # 2.2. Two strings
+    ("char_str", TypeError),        # 2.3. One char, one string
+    ("str_char", TypeError),        # 2.4. One string, one char
+    ("pos_str_pos_str", TypeError), # 2.5. Two strings of positive ints
+    ("neg_str_neg_str", TypeError), # 2.6. Two strings of negative ints
+    ("pos_str_neg_str", TypeError), # 2.7. Two strings of one positive, one negative
+    ("neg_str_pos_str", TypeError), # 2.8. Two strings of one negative, one positive
 ])
 
-# 1.6 All arithmetic w/ COMBO inputs (always result in TypeError)
+# 1.6. All arithmetic w/ COMBO inputs (always result in TypeError)
 COMBO_TEST_CASES = ("arith_combo, expected_combo", [
-    ("pos_int_str", TypeError),     # One positive int, one positive string
-    ("str_pos_int", TypeError),     # One positive string, one positive int
-    ("neg_int_str", TypeError),     # One neg int, one positive string
-    ("str_neg_int", TypeError),     # One string, one neg int
-    ("zero_int_str", TypeError),    # One zero int, one string
-    ("str_zero_int", TypeError),    # One string, one zero int
+    ("pos_int_str", TypeError),     # 3.1. One positive int, one positive string
+    ("str_pos_int", TypeError),     # 3.2. One positive string, one positive int
+    ("neg_int_str", TypeError),     # 3.3. One neg int, one positive string
+    ("str_neg_int", TypeError),     # 3.4. One string, one neg int
+    ("zero_int_str", TypeError),    # 3.5. One zero int, one string
+    ("str_zero_int", TypeError),    # 3.6. One string, one zero int
 ])
 
 ### 2. CALCULATOR OBJECT ###
@@ -93,7 +93,7 @@ def calculator():
 
 ### 3. TESTS ###
 
-### 3.1 INTEGER INPUTS ###
+### 3.1. INTEGER INPUTS ###
 
 # 3.1.1. Addition w/ INT inputs
 @pytest.mark.parametrize(*ADD_INT_TEST_CASES, scope="module")
@@ -127,7 +127,7 @@ def test_division_int(calculator, div_int, expected_div_int, request):
     x, y = request.getfixturevalue(div_int)
     assert calculator.divide(x, y) == expected_div_int
 
-### 3.2 STRING INPUTS ###
+### 3.2. STRING INPUTS ###
 
 # 3.2.1. Addition w/ STR inputs
 @pytest.mark.parametrize(*STRING_TEST_CASES, scope="module")
@@ -161,7 +161,7 @@ def test_division_str(calculator, arith_str, expected_str, request):
     x, y = request.getfixturevalue(arith_str)
     assert calculator.divide(x, y) == expected_str
 
-### 3.3 COMBINATION INPUTS ###
+### 3.3. COMBINATION INPUTS ###
 
 # 3.3.1. Addition w/ COMBO inputs 
 @pytest.mark.parametrize(*COMBO_TEST_CASES, scope="module")
